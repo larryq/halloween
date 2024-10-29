1f5ca0757cf343165e6d8a8f5596d872c5ccecc4
Larry Quantz <lquantz@gmail.com>
2024-10-29 13:56:46 -0700

    first pass at animation

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import * as THREE from 'three'
 import { createRoot } from 'react-dom/client'
-import React, { useRef, useState } from 'react'
+import React, { useRef, useState, useEffect } from 'react'
 import { Canvas, useFrame } from '@react-three/fiber'
 import { getProject } from '@theatre/core'
 import studio from '@theatre/studio'
@@ -8,21 +8,29 @@ import extension from '@theatre/r3f/dist/extension'
 import { editable as e, SheetProvider } from '@theatre/r3f'
 import { PerspectiveCamera } from '@theatre/r3f'
 import { useGLTF, Environment } from '@react-three/drei'
+import demoProjectState from './Demo Project.theatre-project-state.json'
 
 studio.initialize()
 studio.extend(extension)
 
 // our Theatre.js project sheet, we'll use this later
-const demoSheet = getProject('Demo Project').sheet('Demo Sheet')
+//var demoSheet = getProject('Demo Project').sheet('Demo Sheet')
+console.log(demoProjectState)
+const demoSheet = getProject('Demo Project', { state: demoProjectState }).sheet('Demo Sheet')
 
 const App = () => {
+
+  useEffect(() => {
+    demoSheet.project.ready.then(() => demoSheet.sequence.play({ iterationCount: Infinity, range: [0, 4] }))
+  }, [])
   return (
     <Canvas
     >
-       <Environment preset="city" />
-      <SheetProvider sheet={getProject('Demo Project').sheet('Demo Sheet')}>
+      
+      <SheetProvider sheet={demoSheet}>
       <PerspectiveCamera theatreKey="Camera" makeDefault position={[5, 5, -5]} fov={75} />
-      <ambientLight />
+      <ambientLight color={0xfefefe} intensity={0.2}/>
+      <Environment preset="city" />
       <e.pointLight theatreKey="Light" position={[10, 10, 10]} />
       <e.mesh theatreKey='Cube'>
         <boxGeometry args={[1, 1, 1]} />
